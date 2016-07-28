9ff711687f1113a3d32b458420a7b879cd1beb8e
nalarcon18 <nalarcon18@sljhs.org>
2016-07-28 17:05:39 +0000

    Beach.html

--- js/movie.js
+++ js/movie.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     var movie_titles = ["Star Trek Beyond", "Ghostbusters", "Jason Borne", "Nerve", "Secret Life of Pets", "BFG"];
-    var coming_movie_titles = ["Suicide Sqaud", "Maove", "Doctor Strange", Fantastical Beasts and Where to Find Them, Pete´ s Dragon];
+    var coming_movie_titles = ["Suicide Sqaud", "Maove", "Doctor Strange", "Fantastical Beasts and Where to Find Them", "Pete´ s Dragon"];
     var movie = movie_titles[Math.floor(Math.random()*items.length)];
     var coming_title = coming_movie_titles[Math.floor(Math.random()*items.length)];
-    console.log(movie);
-    console.log(coming_title);
+    // console.log(movie);
+    // console.log(coming_title);
+    if (movie == Star Trek Beyond) {
+        $(header).text(Star Trek Beyond)
+    }
 });
