48545ce9c1796aed2ee5dc7d2d4bbed4bab7ec28
nalarcon18 <nalarcon18@sljhs.org>
2016-07-29 15:33:03 +0000

    everything

--- js/food.js
+++ js/food.js
@@ -35,7 +35,8 @@ $(document).ready(function() {
         $(".ip-seven").text("grated lemon zest");
         $(".ip-eight").text(" confectioners' sugar");
         $(".ip-nine").text("blueberries");
-        $(".rp-one").text("");
+        $(".rp-one").text("In a bowl, toss 1 cup raspberries and 1 cup blueberries with 2 tablespoons sugar and 2 tablespoons orange juice; let sit for 5 minutes.
+Dividing evenly, serve 4 split biscuits with 1 pint vanilla ice cream and the berry mixture");
     }
     else if (food == "Fresh Yogurt") {
         var yogurt_img = document.createElement("img");
@@ -44,10 +45,15 @@ $(document).ready(function() {
         _img.setAttribute("Width", "225");
         $(".poster").append(yogurt_img);
         $(".header").text("Fresh Yogurt");
-        $(".ip-one").text(" saucepan");
-         $(".ip-two").text("eggs");
-         
-        $(".rp-one").text("");
+        $(".ip-one").text("milk");
+        $(".ip-two").text("powdered milk");
+        $(".ip-three").text("sugar");
+        $(".ip-four").text("plain yogurt"); 
+        $(".rp-one").text("Pour the milk into a small saucepan and whisk in the powdered milk and honey. Place over medium heat and bring to 120 degrees F on an instant-read thermometer. Once the milk has reached has reached the target temperature, pour it into a cylindrical plastic container, reserving 1/2 cup. Whisk the reserved milk into the yogurt and add it back to the milk mixture.
+Place the container into a narrow wine bucket lined with a heating pad. Set the heating pad to medium. Let the mixture ferment for 3 to 12 hours, making sure the temperature stays as close to 115 degrees F as possible.
+After the fermentation is complete, refrigerate overnight.
+
+Read more at: http://www.foodnetwork.com/recipes/fresh-yogurt-recipe.html?oc=linkback");
     }
     else { chipotle cheesesteak
         var chipotle _img = document.createElement("img");
@@ -56,7 +62,16 @@ $(document).ready(function() {
         chipotle_img.setAttribute("Width", "225");
         $(".poster").append(chipotle_img);
         $(".header").text("Chipotle Cheesesteak");
-        $(".ip-one").text("");
+        $(".ip-one").text("vegetable oil");
+        $(".ip-two").text("pepper-and-onion mixture");
+        $(".ip-three").text("unsalted butter");
+        $(".ip-four").text("all-purpose flour");
+        $(".ip-five").text("warm milk");
+        $(".ip-six").text("ground black pepper");
+        $(".ip-seven").text("cheddar cheese");
+        $(".ip-eight").text(" chopped chipotle peppers in abodo sauce");
+        $(".ip-nine").text("abodo sauce");
+        $(".ip-ten").text("loaf baguette");
         $(".rp-one").text("");
     }
 });
