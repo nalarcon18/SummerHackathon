5948a873114a955575dc055f531eb37442908203
nalarcon18 <nalarcon18@sljhs.org>
2016-07-29 14:28:26 +0000

    everything

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ the person can do in the city for example for the Food section for the person
 it lists a couple of recipes they can do over the summer.
 
 We built this project by using things like bootstrap on the medium 
-screen so that the button would be vertical while on the larger screen the 
+screen so that the buttons would be vertical while on the larger screen the 
 buttons would be horizontal. We used a button tag to make the 
 button work. We linked the buttons for the four pages. In addition bootstarp
 css, javascript, html, and jquery.
@@ -16,10 +16,10 @@ css, javascript, html, and jquery.
 A challenge that we ran into is that in the beginning is that we did have plenty of
 ideas but we just didn't know how to do them. With the idea for this project we didn't
 know how to actually do the code for the this project. But then afte wireframming we
-found out ways to bring the project to life
+found out ways to bring the project to life.
 
 Something that I specfically learned was how to commit to git which I had a bit of trouble during
 the first day as I didn't really understadn where to commit and how to commit. Thankfully two of the 
 instructors taught me how to commit a project to git which I will use later on the future.
 
-For now the next thing for this project is 
+For now the next thing for this project is that 
--- js/food.js
+++ js/food.js
@@ -1,24 +1,47 @@
 $(document).ready(function() {
-   var food_names = ["Berry and ice cream shortcakes"];
-   var food = food_names[Math.floor(Math.random() * food_names.length)];
-   
-   if (food == "") {
-       var _img = document.createElement("img");
-       _img.src = "";
-       _img.setAttribute("height", "350");
-       _img.setAttribute("Width", "225");
-       $(".poster").append(_img);
-       $(".header").text("");
-       $(".ingredients-p").text("");
-       $(".recipe-p").text("");
-   } 
-   else if (food == "") {
-       var _img = document.createElement("img");
-       _img.src = "";
-       _img.setAttribute("height", "350");
-       _img.setAttribute("Width", "225");
-       $(".poster").append(_img);
-       $(".header").text("");
-       $(".recipe").text("");
-   }
+    var food_names = ["Berry and ice cream shortcakes", " Blueberry Tart", "Fresh Yogurt"];
+    var food = food_names[Math.floor(Math.random() * food_names.length)];
+
+    if (food == "Berry and ice cream shortcakes") {
+        var _img = document.createElement("img");
+        _img.src = "";
+        _img.setAttribute("height", "350");
+        _img.setAttribute("Width", "225");
+        $(".poster").append(_img);
+        $(".header").text("");
+        $(".ip-one").text("");
+        // continue this ^^^ up until .ip-ten
+        $(".ir-two").text("");
+        // continue this ^^^ up until .ip-five
+    }
+    else if (food == "") {
+        var _img = document.createElement("img");
+        _img.src = "";
+        _img.setAttribute("height", "350");
+        _img.setAttribute("Width", "225");
+        $(".poster").append(_img);
+        $(".header").text("");
+        $(".ip-one").text("");
+        $(".rp-one").text("");
+    }
+    else if (food == "") {
+        var _img = document.createElement("img");
+        _img.src = "";
+        _img.setAttribute("height", "350");
+        _img.setAttribute("Width", "225");
+        $(".poster").append(_img);
+        $(".header").text("");
+        $(".ip-one").text("");
+        $(".rp-two").text("");
+    }
+    else {
+        var _img = document.createElement("img");
+        _img.src = "";
+        _img.setAttribute("height", "350");
+        _img.setAttribute("Width", "225");
+        $(".poster").append(_img);
+        $(".header").text("");
+        $(".ip-one").text("");
+        $(".rp-two").text("");
+    }
 });
